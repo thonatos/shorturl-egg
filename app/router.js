e23677239a99956cb288fec0fa1ba0fe59560b9c
Thonatos.Yang <thonatos.yang@gmail.com>
2017-07-24 20:40:18 +0800

    fix: redirect api/v1/count to app.count

--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.get('/', 'home.index');
   app.get('/:hash', 'home.redirect');
 
-  app.get('/api/count', 'api.count');
+  app.get('/api/v1/count', 'api.count');
   app.get('/api/v1/expand/:hash', 'api.expand');
   app.post('/api/v1/shorten', 'api.shorten');
 };
