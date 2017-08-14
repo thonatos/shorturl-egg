1bda74bd25c64506c03ffbe8f437be4084f50173
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2017-08-14 17:04:59 +0800

    refactor: remove funcs

--- app/service/shorten.js
+++ app/service/shorten.js
@@ -2,71 +2,38 @@
 
 module.exports = app => {
   class ShortService extends app.Service {
-    * get(query) {
-      const table = app.config.shorturl.table;
-      const result = yield app.mysql.get(table, query);
-      return result;
-    }
-
-    * set(condition) {
-      const table = app.config.shorturl.table;
-      const result = yield app.mysql.insert(table, condition);
-      return result;
-    }
-
-    * del(condition) {
-      const table = app.config.shorturl.table;
-      const result = yield app.mysql.delete(table, condition);
-      return result;
-    }
-
-    * select(condition) {
-      const table = app.config.shorturl.table;
-      const result = yield app.mysql.select(table, condition);
-      return result;
-    }
-
-    * update(condition) {
-      const table = app.config.shorturl.table;
-      const result = yield app.mysql.update(table, condition);
-      return result;
-    }
-
     /**
      * 缩短链接
      * @param {String} url url地址
      * @return {Object} result 由原始url与hash组成的结果
      */
     * shorten(url) {
-      const exist = yield this.get({ url });
-
+      const { table } = app.config.shorturl;
+      const exist = yield app.mysql.get(table, { url });
       if (!exist) {
-        const insert = yield this.set({ url });
+        const insert = yield app.mysql.insert(table, { url });
         const id = insert.insertId;
         const hash = this.ctx.helper.getHashId(id);
-
-        const existHash = yield this.get({ hash });
+        // 检查是否已经存在hash，如果存在则删除本条记录以使得 id 自增
+        const existHash = yield app.mysql.get(table, { hash });
         if (existHash) {
-          yield this.del({ id });
+          yield app.mysql.delete(table, { id });
           const err = new Error('Invalid Hash');
           err.status = 400;
           throw err;
         }
-
-        yield this.update({
+        // 更新 hash
+        yield app.mysql.update(table, {
           id,
           url,
           hash,
         });
-
         return {
           url,
           hash,
         };
       }
-
       const { hash } = exist;
-
       return {
         url,
         hash,
@@ -80,13 +47,10 @@ module.exports = app => {
      * @return {Object} result 数据库中该地址的详细信息
      */
     * expand(hash) {
-      const { cache_prefix, cache_maxAge } = app.config.shorturl;
-
+      const { cache_prefix, cache_maxAge, table } = app.config.shorturl;
       let result = yield app.redis.get(`${cache_prefix}:${hash}`);
-
       if (!result) {
-        result = yield this.get({ hash });
-
+        result = yield app.mysql.get(table, { hash });
         if (result) {
           yield app.redis.set(
             `${cache_prefix}:${hash}`,
@@ -96,21 +60,19 @@ module.exports = app => {
           );
         }
       }
-
       // 如果result是字符串，需要转换成json
       result = typeof result === 'string' ? JSON.parse(result) : result;
-
       return result;
     }
 
     * count(offset = 0, limit = 10) {
+      const { table } = app.config.shorturl;
       const _limit = parseInt(limit) || 10;
-      const result = yield this.select({
+      const result = yield app.mysql.select(table, {
         orders: [[ 'created', 'desc' ], [ 'id', 'desc' ]],
         limit: _limit > 100 ? 100 : _limit,
         offset: parseInt(offset),
       });
-
       return result;
     }
   }
